710a8fd0ddf9285ef293841336307a4339f1743c
Luke Massa <lmassa@tripadvisor.com>
2022-06-04 14:20:27 -0400

    Luke and nick stuff

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
@@ -24,15 +25,56 @@ func getPort() string {
 	return ":3000"
 }
 
+type requestHandler struct {
+	guests map[string]string
+}
+
+type checkResponse struct {
+	Valid bool   `json:"valid"`
+	Form  string `json:"form"`
+}
+
+func getRequestHandler() requestHandler {
+	guests := make(map[string]string)
+	guests["luke"] = "https://tripadvisor.com"
+	guests["nick"] = "https://twitter.com"
+	return requestHandler{
+		guests: guests,
+	}
+}
+
+func (rh requestHandler) CheckGuest(w http.ResponseWriter, r *http.Request) {
+	res := checkResponse{}
+	name := r.URL.Query().Get("name")
+	if val, ok := rh.guests[name]; ok {
+		res.Valid = true
+		res.Form = val
+	}
+	js, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(js)
+
+}
+
 func main() {
+	rh := getRequestHandler()
 	if server {
 		http.HandleFunc("/token", GetToken)
+		http.HandleFunc("/nickandluke/guest", rh.CheckGuest)
 		port := getPort()
 		fmt.Printf("Listening on %s", port)
 		http.ListenAndServe(port, nil)
 	}
 	fmt.Printf(token.GetToken())
 	fmt.Println()
+
+	fmt.Printf("%v", rh)
 }
 
 // GetToken API to get a single token
