2600f52c0dcb566b86e4c128288c9525ae036bd5
Luke Massa <lmassa@tripadvisor.com>
2022-08-03 23:43:48 -0400

    Move to using S3

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lukemassa/nickandluke-api
 
 go 1.14
 
-require github.com/jessevdk/go-flags v1.5.0
+require (
+	github.com/aws/aws-sdk-go v1.44.69
+	github.com/jessevdk/go-flags v1.5.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/jessevdk/go-flags"
 	nickandluke "github.com/lukemassa/nickandluke-api/internal/nickandluke"
 )
@@ -19,20 +22,39 @@ func getPort() string {
 
 func main() {
 	var opts struct {
-		Action string `long:"action" required:"true" default:"run" choice:"server" choice:"validate" choice:"upload"`
+		Action string `long:"action" required:"true" default:"run" choice:"server" choice:"validate" choice:"download" choice:"download-and-server"`
 	}
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		os.Exit(1)
 	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: credentials.NewEnvCredentials(),
+	})
+
+	if err != nil {
+		panic(err)
+	}
 	rh := nickandluke.RequestHandler()
-	if opts.Action == "server" {
+	dh := nickandluke.DataHandler(sess)
+	if opts.Action == "download" || opts.Action == "download-and-server" {
+		err := dh.Download()
+		if err != nil {
+			panic(err)
+		}
+	}
+	if opts.Action == "server" || opts.Action == "download-and-server" {
 		http.HandleFunc("/guest", rh.CheckGuest)
 		port := getPort()
 		fmt.Printf("Listening on %s", port)
 		panic(http.ListenAndServe(port, nil))
 
 	}
-	fmt.Println()
-	fmt.Printf("%v", rh)
+	if opts.Action == "validate" {
+		fmt.Println()
+		fmt.Printf("%v", rh)
+	}
+
 }
