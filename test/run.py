e3d55946c4d5492ef1c81f858e3023434bf981de
Luke Massa <lmassa@tripadvisor.com>
2022-08-03 23:43:48 -0400

    Move to using S3

--- test/run.py
+++ test/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import requests
+import argparse
 
 BASE_URL="https://nickandluke-api.herokuapp.com"
 
@@ -15,10 +16,18 @@ def one_test(url, expected_response):
 
 
 def main():
+    base_url = BASE_URL
 
-    one_test(f"{BASE_URL}/guest?name=foobar", {"valid":False,"form":""})
-    one_test(f"{BASE_URL}/guest?name=vincent+massa", {'valid': True, 'form': 'https://docs.google.com/forms/d/e/1FAIpQLSevxS_HMScw6Nhcru3ke8GeqWfJnBAA_AdWPc-1eRmgS4G6LQ/viewform?usp=sf_link'})
-    one_test(f"{BASE_URL}/guest?name=nancy+massa", {'valid': True, 'form': 'https://docs.google.com/forms/d/e/1FAIpQLSdXF80AevtDqkC7ZTynrzXRuwfZCjQPTpsLhCEfuRPSOCCgww/viewform?usp=sf_link'})
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--localhost",action="store_true",default=False)
+    args = parser.parse_args()
+
+    if args.localhost:
+        base_url = "http://localhost:3000"
+
+    one_test(f"{base_url}/guest?name=foobar", {"valid":False,"form":""})
+    one_test(f"{base_url}/guest?name=vincent+massa", {'valid': True, 'form': 'https://docs.google.com/forms/d/e/1FAIpQLSevxS_HMScw6Nhcru3ke8GeqWfJnBAA_AdWPc-1eRmgS4G6LQ/viewform?usp=sf_link'})
+    one_test(f"{base_url}/guest?name=nancy+massa", {'valid': True, 'form': 'https://docs.google.com/forms/d/e/1FAIpQLSdXF80AevtDqkC7ZTynrzXRuwfZCjQPTpsLhCEfuRPSOCCgww/viewform?usp=sf_link'})
 
 if __name__ == "__main__":
     main()
